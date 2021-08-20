e63f7cd0c4b9ef20cd6c1bb39b8efffd4c22c324
joplang <joplang@hotmail.com>
2021-08-20 15:20:08 +0200

    first commit

---
+++ database/factories/ProductDetailsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProductDetails;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductDetailsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ProductDetails::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
